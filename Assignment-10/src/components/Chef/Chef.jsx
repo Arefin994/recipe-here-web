11b61dcd9c8c372ac4d6a8d532fc90ab9a81370d
Arefin994 <arefinamin994@gmail.com>
2023-05-03 21:07:42 +0600

    Finally loaded data from server

--- Assignment-10/src/components/Chef/Chef.jsx
+++ Assignment-10/src/components/Chef/Chef.jsx
@@ -5,21 +5,25 @@ const Chef = () => {
 
     console.log(chefData)
     return (
-        <div>
-            <h2>{chefData.name}</h2>
-            <img src={chefData.picture} alt={chefData.name} />
-            <p>Experience: {chefData.experience} years</p>
-            <h3>Recipes:</h3>
-            {chefData.recipes.map((recipe, index) => (
-                <div key={index}>
-                    <h4>{recipe.name}</h4>
-                    <p>Ingredients: {recipe.ingredients.join(', ')}</p>
-                    <p>Method: {recipe.method}</p>
-                    <p>Rating: {recipe.rating}</p>
-                    <p>Favourite: {recipe.favourite ? 'Yes' : 'No'}</p>
+        <div className="container">
+            <div className="row">
+                <div className="col-md-6">
+                    <h2 className="text-primary">{chefData.name}</h2>
+                    <img src={chefData.picture} alt={chefData.name} className="img-fluid rounded" />
+                    <p className="mt-3">Experience: {chefData.experience} years</p>
+                    <h3 className="mt-4">Recipes:</h3>
+                    {chefData.recipes.map((recipe, index) => (
+                        <div key={index} className="mt-3">
+                            <h4>{recipe.name}</h4>
+                            <p>Ingredients: {recipe.ingredients.join(', ')}</p>
+                            <p>Method: {recipe.method}</p>
+                            <p>Rating: {recipe.rating}</p>
+                            <p>Favourite: {recipe.favourite ? 'Yes' : 'No'}</p>
+                        </div>
+                    ))}
+                    <p className="mt-4">Likes: {chefData.likes}</p>
                 </div>
-            ))}
-            <p>Likes: {chefData.likes}</p>
+            </div>
         </div>
     );
 };
--- Assignment-10/src/components/Home/Home.jsx
+++ Assignment-10/src/components/Home/Home.jsx
@@ -1,21 +1,22 @@
 import React, { useEffect, useState } from "react";
 import Recipes from "../Recipes/Recipes"
 import Chefs from "../Chefs/Chefs";
+import './Home.css';
 
 const Home = () => {
-  const [recipeData, setRecipeData] = useState([]);
-
-
+  const [chef, setChef] = useState([]);
   useEffect(() => {
-    fetch("http://localhost:5000/allData")
+    fetch('http://localhost:5000/allData')
       .then(res => res.json())
-      .then(data => setRecipeData(data.recipes))
-      .catch((error) => console.log(error));
-  }, []);
-
-  console.log(recipeData)
-
-
+      .then(data => setChef(data));
+    console.log("home")
+    console.log({ chef })
+    { console.log(chef) }
+  }, [])
+  const handleViewRecipes = (chef) => {
+    localStorage.setItem('selectedChef', JSON.stringify(chef));
+    location.href = '/chef';
+  };
   return (
     <div>
       <div className="w-75 h-75 mx-auto my-auto">
@@ -60,9 +61,20 @@ const Home = () => {
       </div>
 
       <div>
-        <h3 className="text-center">Latest Recipes</h3>
+        <h3 className="text-center mt-4">Our Top <span className="thm-clr">Chefs</span></h3>
         <div>
-          <Chefs></Chefs>
+          {chef.allItems?.chefs.map((chef, index) => (
+            <div key={chef.name} className="chef-card">
+              <img src={chef.picture} alt={chef.name} className="chef-image" />
+              <div className="chef-details">
+                <div className="chef-name">{chef.name}</div>
+                <div className="chef-experience">{chef.experience} years of experience</div>
+                <div className="chef-recipes">{chef.recipes.length} recipes</div>
+                <div className="chef-likes">{chef.likes} likes</div>
+                <button className="view-recipes-btn" onClick={() => handleViewRecipes(chef)}>View Recipes</button>
+              </div>
+            </div>
+          ))}
         </div>
       </div>
     </div>
