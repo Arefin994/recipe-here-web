efc99e3cc419098de022fb5403767e6a1383f4f3
Arefin994 <arefinamin994@gmail.com>
2023-05-03 12:35:03 +0600

    made /chef somewhat working

--- Assignment-10/src/components/Chef/Chef.jsx
+++ Assignment-10/src/components/Chef/Chef.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 
 const Chef = () => {
+    const chefData = JSON.parse(localStorage.getItem('selectedChef'));
+
+    console.log(chefData)
     return (
         <div>
-            A Chef
+            <h2>{chefData.name}</h2>
+            <img src={chefData.picture} alt={chefData.name} />
+            <p>Experience: {chefData.experience} years</p>
+            <h3>Recipes:</h3>
+            {chefData.recipes.map((recipe, index) => (
+                <div key={index}>
+                    <h4>{recipe.name}</h4>
+                    <p>Ingredients: {recipe.ingredients.join(', ')}</p>
+                    <p>Method: {recipe.method}</p>
+                    <p>Rating: {recipe.rating}</p>
+                    <p>Favourite: {recipe.favourite ? 'Yes' : 'No'}</p>
+                </div>
+            ))}
+            <p>Likes: {chefData.likes}</p>
         </div>
     );
 };
