d3ef1f7b6cdc0e24c4e79c7875eb80ae997a6c03
Arefin994 <arefinamin994@gmail.com>
2023-05-05 23:18:10 +0600

    Made chef recipe login protected.

--- Assignment-10/src/components/Chef/Chef.jsx
+++ Assignment-10/src/components/Chef/Chef.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Chef.css';
 import Footer from '../Footer/Footer';
 import { ToastContainer, toast } from 'react-toastify';
@@ -6,10 +6,25 @@ import 'react-toastify/dist/ReactToastify.css';
 
 import { FaRegStar, FaStar } from 'react-icons/fa';
 import Rating from 'react-rating';
+import { getAuth, onAuthStateChanged } from 'firebase/auth';
+import app from '../../firebase/firebase.config';
+
+const auth = getAuth(app);
 
 const Chef = () => {
+  const [user, setUser] = useState(null);
   const chefData = JSON.parse(localStorage.getItem('selectedChef'));
   const [recipes, setRecipes] = useState(chefData.recipes);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser(null);
+      }
+    });
+  }, []);
 
   const handleFav = (index) => {
     const updatedRecipes = [...recipes];
@@ -23,68 +38,70 @@ const Chef = () => {
     location.href = '/recipe';
   };
 
-  console.log(chefData);
   return (
     <div className="container">
       <ToastContainer />
-      <div>
+      <h2 className="thm-clr">{chefData.name}</h2>
+      <div className="d-flex">
         <div>
-          <h2 className="thm-clr">{chefData.name}</h2>
-          <div className="d-flex">
-            <div>
-              <img
-                src={chefData.picture}
-                alt={chefData.name}
-                className="chef-img img-fluid rounded-5 shadow-lg"
-              />
-            </div>
-            <div className="my-auto px-4">
-              <p className="mt-3">Experience: {chefData.experience} years</p>
-              <p className="mt-4">Likes: {chefData.likes}</p>
-            </div>
-          </div>
+          <img
+            src={chefData.picture}
+            alt={chefData.name}
+            className="chef-img img-fluid rounded-5 shadow-lg"
+          />
+        </div>
+        <div className="my-auto px-4">
+          <p className="mt-3">Experience: {chefData.experience} years</p>
+          {user && <p className="mt-4">Likes: {chefData.likes}</p>}
+        </div>
+      </div>
 
-          <h3 className="mt-4">Recipes:</h3>
-          {recipes.map((recipe, index) => (
-            <div key={index} className="mt-3">
-              <div className="recipe-box shadow-lg w-100">
-                <div className="d-flex justify-content-between">
-                  <div className="d-flex flex-grow-1">
-                    <div className="my-auto"><img className='food-img' src={recipe.image} alt="nai" /></div>
-                    <div>
-                      <h4>{recipe.name}</h4>
-                      <div className='d-flex'>
-                        <div>
-                          <p>Rating: </p>
-                        </div>
-                        <div className='mx-1'>
-                          <Rating
-                            placeholderRating={recipe.rating}
-                            readonly
-                            emptySymbol={<FaRegStar></FaRegStar>}
-                            placeholderSymbol={<FaStar></FaStar>}
-                            fullSymbol={<FaStar />}
-                          />
-                        </div>
+      <h3 className="mt-4">Recipes:</h3>
+      {user ? (
+        recipes.map((recipe, index) => (
+          <div key={index} className="mt-3">
+            <div className="recipe-box shadow-lg w-100">
+              <div className="d-flex justify-content-between">
+                <div className="d-flex flex-grow-1">
+                  <div className="my-auto"><img className='food-img' src={recipe.image} alt="nai" /></div>
+                  <div>
+                    <h4>{recipe.name}</h4>
+                    <div className='d-flex'>
+                      <div>
+                        <p>Rating: </p>
+                      </div>
+                      <div className='mx-1'>
+                        <Rating
+                          placeholderRating={recipe.rating}
+                          readonly
+                          emptySymbol={<FaRegStar></FaRegStar>}
+                          placeholderSymbol={<FaStar></FaStar>}
+                          fullSymbol={<FaStar />}
+                        />
+                      </div>
 
-                        <div className='mx-1'>
-                          <p>{recipe.rating}</p>
-                        </div>
+                      <div className='mx-1'>
+                        <p>{recipe.rating}</p>
                       </div>
                     </div>
                   </div>
-                  <div className="my-auto">
-                    {!recipe.favourite && (
-                      <button className="thm-bg-clr text-white" onClick={() => handleFav(index)}>Favourite</button>
-                    )}
-                    <button className='thm-bg-clr text-white mx-2' onClick={() => handleViewRecipe(recipe)}>View recipe</button>
-                  </div>
+                </div>
+                <div className="my-auto">
+                  {!recipe.favourite && (
+                    <button className="thm-bg-clr text-white" onClick={() => handleFav(index)}>Favourite</button>
+                  )}
+                  <button className='thm-bg-clr text-white mx-2' onClick={() => handleViewRecipe(recipe)}>View recipe</button>
                 </div>
               </div>
             </div>
-          ))}
+          </div>
+        ))
+      ) : (
+        <div className="text-center border shadow-sm py-3">
+          <p className="fs-5">Please login to gain full access to recipes</p>
+          <button className="btn thm-bg-clr text-white" onClick={() => location.href = '/login'}>Login</button>
         </div>
-      </div>
+      )}
       <Footer></Footer>
     </div>
   );
